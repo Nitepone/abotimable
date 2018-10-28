848b74099dbff072ccfb53683bf51efd816ed468
cjb5326 <cjb5326@cs.rit.edu>
2018-10-28 01:08:27 -0400

    - PEP8 style updates - Additonal team_rtm changes

--- abotimable/testmodule.py
+++ abotimable/testmodule.py
@@ -4,13 +4,14 @@ Test module that simply prints recieved notifications
 
 from .teamBotModule import TeamBotModule
 
+
 class TestModule:
 
     def __init__(self):
         pass
 
-
-    def notify_message(teamRTM, message):
+    def notify_message(self, slack_client, message):
         print(message.text)
+
 
 TeamBotModule.register(TestModule)
