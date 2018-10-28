4074cb548f4ca139dec813542d432f991f622862
Elliot Miller <elliot@bitoffdev.com>
2018-10-28 08:28:05 -0400

    Add thread restarting

--- abotimable/__main__.py
+++ abotimable/__main__.py
@@ -1,13 +1,24 @@
 import threading
+import logging
 
 from . import server
 from . import slackrtm
 
-server_thread = threading.Thread(target=server.app.run, kwargs={
-    "host": "0.0.0.0",
-    "port": 5000
-})
-server_thread.start()
+def server_monitor():
+    while True:
+        try:
+            server.app.run(host="0.0.0.0", port=5000)
+        except Exception as e:
+            logging.error("Server thread died")
+            logging.error(e)
 
-slack_thread = threading.Thread(target=slackrtm.main)
-slack_thread.start()
+def slack_monitor():
+    while True:
+        try:
+            slackrtm.main()
+        except Exception as e:
+            logging.error("Slack thread died")
+            logging.error(e)
+
+threading.Thread(target=slack_monitor).start()
+threading.Thread(target=server_monitor).start()
