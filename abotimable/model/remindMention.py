eee169eb377438e86f3a072c2e1816a1141885d1
cjb5326 <cjb5326@cs.rit.edu>
2018-10-28 02:57:22 -0400

    - Completed inital code for remindMention.py

--- abotimable/model/remindMention.py
+++ abotimable/model/remindMention.py
@@ -6,6 +6,7 @@
     Description: Reminds the recipient of a mention that they've been mentioned.
 """
 from ..teamBotModule import TeamBotModule
+import re
 
 
 class RemindMention():
@@ -13,17 +14,31 @@ class RemindMention():
     def __init__(self):
         pass
 
-    def check_for_mention(message):
+    def check_for_mention(self, message):
         if "@" in message:
             return True
         else:
             return False
 
-    def process_payload(payload):
-        return
+    def process_payload(self, slack_client, message):
+        nonsplit_user = re.findall(r'@.[a-z]*', message)
+        user = re.split('@', nonsplit_user[0])[1]
+        create_dm_request = slack_client.api_call(
+            "conversations.open",
+            users=[user]
+        )
+        if create_dm_request['ok'] == True:
+            dm_channel_id = create_dm_request['channel']['id']
+            send_dm_message = slack_client.api_call(
+                "chat.postMessage",
+                channel = dm_channel_id,
+                text = "Hey! " + nonsplit_user[0] + " sent you a message!"
+            )
+
 
     def notify_message(self, slack_client, message):
-        pass
+        if self.check_for_mention(message):
+            self.process_payload(slack_client, message)
 
 
 TeamBotModule.register(RemindMention)
