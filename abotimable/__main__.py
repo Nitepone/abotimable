db6b05410ccba26107eca72cefb5a9a2386ea1b3
Elliot Miller <elliot@bitoffdev.com>
2018-10-28 10:15:51 -0400

    Add song lyrics initial integration

--- abotimable/__main__.py
+++ abotimable/__main__.py
@@ -15,15 +15,6 @@ def server_monitor():
         logging.info("Waiting 5 seconds before restarting server thread")
         time.sleep(5)
 
-def slack_monitor():
-    while True:
-        try:
-            slackrtm.main()
-        except Exception as e:
-            logging.error("Slack thread died")
-            logging.error(e)
-        logging.info("Waiting 5 seconds before restarting slack thread")
-        time.sleep(5)
-
-threading.Thread(target=slack_monitor).start()
 threading.Thread(target=server_monitor).start()
+
+slackrtm.main()
