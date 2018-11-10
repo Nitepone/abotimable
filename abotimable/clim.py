d0123438e1459c3ccfe02cd4a404d62bcf045bd3
cjb5326 <cjb5326@cs.rit.edu>
2018-11-09 23:45:33 -0500

    - Adds "you got clim'd!" message

--- abotimable/clim.py
+++ abotimable/clim.py
@@ -1,6 +1,6 @@
 """
     File: clim.py
-    Version: 1.0.0
+    Version: 1.0.1
     Author: Chris Baudouin, Jr.
 
     Description: All clim-related features including but not limited to: "you got clim'd!", climtistics and more.
@@ -9,6 +9,7 @@ from .teamBotModule import TeamBotModule
 from .model.message import Message
 from slackclient import SlackClient
 import logging
+import re
 
 logger = logging.getLogger(__name__)
 
@@ -18,8 +19,24 @@ class Clim:
     def __init__(self):
         pass
 
+    def its_clim(self, message : Message) -> bool:
+        if "@" in message.text and message.user == "UDQAXS6UD":
+            return True
+        else:
+            return False
+
+    def clim_em(self, slack_client, message):
+        recipients = re.findall(r'@[a-zA-Z0-9]*', message.text)
+        for recipient in recipients:
+            message_response = slack_client.api_call(
+                "chat.postMessage",
+                channel=message.channel,
+                text="_<@{}> you just got clim'd!_".format(str(recipient).strip("@"))
+            )
+
     def notify_message(self, slack_client: SlackClient, message: Message) -> None:
-        pass
+        if self.its_clim(message):
+            self.clim_em(slack_client, message)
 
 
 TeamBotModule.register(Clim)
